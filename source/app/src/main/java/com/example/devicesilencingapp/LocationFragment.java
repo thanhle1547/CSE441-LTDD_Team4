e1a418b80a02355ef3929f68039830499f527203
= <tvtu165@gmail.com>
2020-05-11 00:50:44 +0700

    DIALOG ADD AND CLICK ITEM LOCATION

--- source/app/src/main/java/com/example/devicesilencingapp/LocationFragment.java
+++ source/app/src/main/java/com/example/devicesilencingapp/LocationFragment.java
@@ -1,19 +1,27 @@
 package com.example.devicesilencingapp;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class LocationFragment extends Fragment {
+public class LocationFragment extends Fragment{
     Context context;
     private ListView lv_Location;
     private AdapterLocation adapter;
@@ -34,6 +42,55 @@ public class LocationFragment extends Fragment {
         context = getActivity();
         adapter = new AdapterLocation(context, list);
         lv_Location.setAdapter(adapter);
+
+
+
+
+        lv_Location.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                MDLocation location =(MDLocation)list.get(i);
+                Toast.makeText(context, "getDiadiem"+ location.getDiadiem() + "getImg"+ location.getImg(), Toast.LENGTH_SHORT).show();
+                dialogclickitemlocation(location.getDiadiem());
+
+                //                Intent intent = new Intent(context, class.class);
+//                intent.putExtra("getDiadiem", location.getDiadiem());
+//                intent.putExtra("getImg", location.getImg());
+//                intent.putExtra("getTendiadiem", location.getTendiadiem());
+//                intent.putExtra("getTrangthai", location.getTrangthai());
+//                startActivity(intent);
+            }
+        });
         return view;
     }
+    private void dialogclickitemlocation(String a){
+        Dialog dialogclickitem= new Dialog(getActivity());
+        dialogclickitem.setContentView(R.layout.clickitemlocation);
+        final ImageView battatLocation = (ImageView)dialogclickitem.findViewById(R.id.battatLocation);
+        final LinearLayout sualocation = (LinearLayout)dialogclickitem.findViewById(R.id.sualocation);
+        final LinearLayout xoalocation = (LinearLayout)dialogclickitem.findViewById(R.id.xoalocation);
+        battatLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "bat tat", Toast.LENGTH_SHORT).show();
+            }
+        });
+        sualocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "sua", Toast.LENGTH_SHORT).show();
+            }
+        });
+        xoalocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "xoa", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Window window = dialogclickitem.getWindow();
+        WindowManager.LayoutParams wlp = window.getAttributes();
+        wlp.gravity = Gravity.BOTTOM;
+        dialogclickitem.show();
+    }
+
 }
--- source/app/src/main/java/com/example/devicesilencingapp/MainActivity.java
+++ source/app/src/main/java/com/example/devicesilencingapp/MainActivity.java
@@ -4,23 +4,71 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener;
 
 public class MainActivity extends AppCompatActivity {
 
+	ImageButton clickadd;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		clickadd = (ImageButton)findViewById(R.id.btclickadd);
+		clickadd.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				DialogAdd();
+			}
+		});
 		BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
 		bottomNav.setOnNavigationItemSelectedListener(navListener);
 		getSupportFragmentManager().beginTransaction().replace(R.id.fragment_main, new LocationFragment()).commit();
 	}
+	private void DialogAdd(){
+		final Dialog dialog = new Dialog(this);
+		dialog.setContentView(R.layout.clickadd_fragment);
+		final LinearLayout addvtht = (LinearLayout)dialog.findViewById(R.id.addvtht);
+		final LinearLayout adddd = (LinearLayout)dialog.findViewById(R.id.adddiadiem);
+		final LinearLayout addtime = (LinearLayout)dialog.findViewById(R.id.addtime);
+		addvtht.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Toast.makeText(MainActivity.this, "tạo intent add vi tri hien tai", Toast.LENGTH_SHORT).show();
+			}
+		});
+		adddd.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Toast.makeText(MainActivity.this, "tạo intent add dia diem moi", Toast.LENGTH_SHORT).show();
+			}
+		});
+		addtime.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Toast.makeText(MainActivity.this, "tạo intent add time", Toast.LENGTH_SHORT).show();
+			}
+		});
+		Window window = dialog.getWindow();
+		WindowManager.LayoutParams wlp = window.getAttributes();
+		wlp.gravity = Gravity.BOTTOM | Gravity.RIGHT;
+		wlp.x = 40;   //x position
+		wlp.y = 90;   //y position
+		dialog.show();
+	}
+
 	private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener(){
 		@Override
 		public boolean onNavigationItemSelected(@NonNull MenuItem item) {
