70dabebc9e0e972a37e80117d2b6a3e3ea77aad4
= <tvtu165@gmail.com>
2020-05-18 20:19:18 +0700

    add location fragment

--- source/app/src/main/java/com/example/devicesilencingapp/location/fragments/LocationDetailFragment.java
+++ source/app/src/main/java/com/example/devicesilencingapp/location/fragments/LocationDetailFragment.java
@@ -1,16 +1,15 @@
 package com.example.devicesilencingapp.location.fragments;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.devicesilencingapp.R;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -44,7 +43,6 @@ public class LocationDetailFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        }
     }
 
     @Override
